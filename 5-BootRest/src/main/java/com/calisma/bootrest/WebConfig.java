2f20ed52ad04a2b340faaa99d5d4136362da3d87
Hakan ÖZER <hakanozer02@gmail.com>
2018-12-14 12:05:06 +0300

    Bean

---
+++ 5-BootRest/src/main/java/com/calisma/bootrest/WebConfig.java
@@ -0,0 +1,26 @@
+package com.calisma.bootrest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.calisma.bootrest")
+public class WebConfig implements WebMvcConfigurer{
+	
+	@Bean(name = "dataSource")
+	public DriverManagerDataSource datasource() {
+		DriverManagerDataSource dt = new DriverManagerDataSource();
+		dt.setDriverClassName("com.mysql.jdbc.Driver");
+		dt.setUrl("jdbc:mysql://localhost/springdb?useUnicode=true&characterEncoding=utf-8");
+		dt.setUsername("root");
+		dt.setPassword("");
+		return dt;
+	}
+		
+
+}
