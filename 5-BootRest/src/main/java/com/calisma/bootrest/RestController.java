cd12177d675e69fd14eb34814ef03c875e1b2590
Hakan ÖZER <hakanozer02@gmail.com>
2018-12-14 11:47:42 +0300

    Rest

---
+++ 5-BootRest/src/main/java/com/calisma/bootrest/RestController.java
@@ -0,0 +1,74 @@
+package com.calisma.bootrest;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+	
+	DB db = new DB();
+
+	@RequestMapping(value = "/allUser", method = RequestMethod.GET)
+	public HashMap<String, Object> allUser() {
+		HashMap<String, Object> hm = new HashMap<>();
+		hm.put("users", dataResult());
+		return hm;
+	}
+	
+	
+	public List<User> dataResult() {
+		List<User> ls = new ArrayList<User>();
+		try {
+			String query = "select * from user";
+			ResultSet rs = db.preBaglan(query).executeQuery();
+			while(rs.next()) {
+				User us = new User();
+				us.setUid(rs.getInt("uid"));
+				us.setName(rs.getString("name"));
+				us.setSurname(rs.getString("surname"));
+				us.setMail(rs.getString("mail"));
+				us.setPass(rs.getString("pass"));
+				ls.add(us);
+			}
+		} catch (Exception e) {
+			System.err.println("Hata : " + e);
+		}
+		return ls;
+	}
+	
+	
+	// insert user
+	@RequestMapping(value = "/insert", method = RequestMethod.POST)
+	public HashMap<String, Object> insert(@RequestBody User us) {
+		HashMap<String, Object> hm = new HashMap<>();
+		try {
+			String query = "insert into user values(null, ?, ?, ?, ?)";
+			PreparedStatement pre = db.preBaglan(query);
+			pre.setString(1, us.getName());
+			pre.setString(2, us.getSurname());
+			pre.setString(3, us.getMail());
+			pre.setString(4, DB.MD5(us.getPass()));
+			int statu = pre.executeUpdate();
+			if(statu > 0) {
+				hm.put("statu", true);
+				hm.put("message", "Ekleme işlemi başarılı");
+			}else {
+				hm.put("statu", false);
+				hm.put("message", "Ekleme işlemi sırasında bir hata oluştu");
+			}
+		} catch (Exception e) {
+			System.err.println("Hata : " + e);
+		}
+		return hm;
+	}
+	
+	
+	
+}
---
+++ 5-BootRest/src/main/java/com/calisma/bootrest/User.java
@@ -0,0 +1,46 @@
+package com.calisma.bootrest;
+
+public class User {
+
+	private int uid;
+	private String name;
+	private String surname;
+	private String mail;
+	private String pass;
+	
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	
+	
+	
+	
+}
